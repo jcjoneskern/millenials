aa4988bd2868c8b8e5e5a18b411f175dc42e90e8
J-C <jcjoneskern@gmail.com>
2017-02-26 16:23:42 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  $("button").on("click", function() {
+    console.log($("#headline").text());
+  });
+
+});
+
+// 1. get button working x
+// 2. have button make any change at all
+// 3. change stuff with pieces from array
+
+// gonna need .text()
+
+
+// Millenials are xing y because z
+// Millenials are xing y with z
