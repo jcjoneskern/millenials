1717cf1cba55e7a4354e8ec18b1788eb8fc09dc0
J-C <jcjoneskern@gmail.com>
2017-02-26 18:57:06 -0500

    typo correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function() {
-  const verbs = ["killing", "ruining", "eating", "ending", "aren't buying", "getting rid of"];
+  const verbs = ["killing", "ruining", "eating", "ending", "not buying", "getting rid of"];
   const nouns = ["email", "napkins", "diamonds", "houses", "golf", "typewriters", "yachts", "capitalism", "the police", "God", "razors", "avocados", "marriage", "the government", "cubicles", "savings accounts", "voting", "waiting 'til marriage", "Republicans", "this generation", "wine", "cereal"];
   const reasons = ["they're lazy", "they're entitled", "of socialism", "they hate success", "they hate themselves", "they buy too much kale", "they take too many vacations", "they don't take enough vacations", "I said so", "they were given participation trophies", "they're gay", "of smartphones"];
   const things = ["entitlement", "communism", "kale", "excessive vacations", "social media", "selfies", "memes", "feelings", "participation trophies", "crowdfunding", "sexting", "the homosexual agenda", "the liberal agenda", "smartphones"]
