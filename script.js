411d9ea8d856b40b55a5d00a486b2273402ab9f9
J-C <jcjoneskern@gmail.com>
2017-02-26 17:15:22 -0500

    basic sentences working

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 $(function() {
+  const verbs = ["killing", "ruining", "eating", "obliterating", "aren't buying"];
+  const nouns = ["napkins", "diamonds", "houses", "golf", "typewriters", "yachts", "capitalism", "the police", "God", "razors", "avocados"];
+  const reasons = ["they're lazy", "they're entitled", "communism", "they hate success", "they hate themselves", "they buy too much kale", "they take too many vacations", "they don't take enough vacations"];
+  // const headlines = ["Millenials are " + verb() + " " + noun() + " because " + reason(), "Millenials are " + verb() + " " + noun() + " with " + reason()];
+
+  function getHeadline() {
+    let verb = function() {
+      return verbs[Math.floor(Math.random()*verbs.length)];
+    }
+    let noun = function() {
+      return nouns[Math.floor(Math.random()*nouns.length)];
+    }
+    let reason = function() {
+      return reasons[Math.floor(Math.random()*reasons.length)];
+    }
+
+    let headline = "Millenials are " + verb() + " " + noun() + " because " + reason();
+    return headline;
+  }
 
   $("button").on("click", function() {
-    console.log($("#headline").text());
+    let headline = getHeadline();
+    $("#headline").text(headline);
   });
 
 });
 
-// 1. get button working x
-// 2. have button make any change at all
-// 3. change stuff with pieces from array
-
-// gonna need .text()
-
 
 // Millenials are xing y because z
 // Millenials are xing y with z
+// Millenials hate y because z
+// Millenials can't a because z
