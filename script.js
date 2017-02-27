f299ae2fd99d2f8f8d390a5b1e58a0fb8c20101f
J-C <jcjoneskern@gmail.com>
2017-02-26 21:21:54 -0500

    adding more

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function() {
   const verbs = ["killing", "ruining", "eating", "ending", "not buying", "getting rid of"];
-  const nouns = ["email", "napkins", "diamonds", "houses", "golf", "typewriters", "yachts", "capitalism", "the police", "God", "razors", "avocados", "marriage", "the government", "cubicles", "savings accounts", "voting", "waiting 'til marriage", "Republicans", "this generation", "wine", "cereal"];
-  const reasons = ["they're lazy", "they're entitled", "of socialism", "they hate success", "they hate themselves", "they buy too much kale", "they take too many vacations", "they don't take enough vacations", "I said so", "they were given participation trophies", "they're gay", "of smartphones"];
+  const nouns = ["email", "napkins", "diamonds", "houses", "golf", "typewriters", "yachts", "capitalism", "the police", "God", "razors", "avocados", "marriage", "the government", "cubicles", "savings accounts", "voting", "waiting 'til marriage", "Republicans", "this generation", "wine", "cereal", "bar soap", "basic human decency"];
+  const reasons = ["they're lazy", "they're entitled", "of socialism", "they hate success", "they hate themselves", "they take too many vacations", "they don't take enough vacations", "I said so", "they were given participation trophies", "they're gay", "of smartphones", "of SJWs"];
   const things = ["entitlement", "communism", "kale", "excessive vacations", "social media", "selfies", "memes", "feelings", "participation trophies", "crowdfunding", "sexting", "the homosexual agenda", "the liberal agenda", "smartphones"]
 
   function getHeadline() {
