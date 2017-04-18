8675109aa8f8afac8b31fad4c43f25b715669c5a
J-C <jcjoneskern@gmail.com>
2017-04-18 11:34:10 -0400

    added more stuff

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(function() {
-  const verbs = ["killing", "ruining", "eating", "ending", "not buying", "getting rid of"];
-  const nouns = ["email", "napkins", "diamonds", "houses", "golf", "typewriters", "yachts", "capitalism", "the police", "God", "razors", "marriage", "the government", "cubicles", "savings accounts", "voting", "waiting 'til marriage", "Republicans", "this generation", "wine", "cereal", "bar soap", "basic human decency", "the sanctity of marriage"];
-  const reasons = ["they're lazy", "they're entitled", "of socialism", "they hate success", "they hate themselves", "they take too many vacations", "they don't take enough vacations", "I said so", "they were given participation trophies", "they're gay", "of smartphones", "of SJWs", "they live with their parents", "they leak government secrets"];
-  const things = ["entitlement", "communism", "kale", "excessive vacations", "social media", "selfies", "memes", "feelings", "participation trophies", "crowdfunding", "sexting", "the homosexual agenda", "the liberal agenda", "smartphones", "Pepe", "hashtags", "avocados"]
+  const verbs = ["killing", "ruining", "eating", "ending", "not buying", "getting rid of", "not having"];
+  const nouns = ["email", "napkins", "diamonds", "houses", "golf", "capitalism", "the police", "God", "razors", "marriage", "the government", "cubicles", "savings accounts", "voting", "waiting 'til marriage", "Republicans", "this generation", "wine", "cereal", "bar soap", "basic human decency", "the sanctity of marriage", "in-person communication", "children"];
+  const reasons = ["they're lazy", "they're entitled", "of socialism", "they hate success", "they hate themselves", "they take too many vacations", "they don't take enough vacations", "I said so", "they were given participation trophies", "they're gay", "of smartphones", "of SJWs", "they live with their parents", "they leak government secrets", "they watch too much Netflix"];
+  const things = ["entitlement", "communism", "kale", "excessive vacations", "social media", "selfies", "memes", "feelings", "participation trophies", "crowdfunding", "sexting", "the homosexual agenda", "the liberal agenda", "smartphones", "Pepe", "hashtags", "avocados", "emojis", "Netflix and chill"];
 
   function getHeadline() {
     let verb = function() {
