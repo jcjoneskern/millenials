7178980d755a1d5c6dabc8622b3984f002e52d31
J-C <jcjoneskern@gmail.com>
2017-05-10 10:44:19 -0400

    refactored

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ $(function() {
   const reasons = ["they're lazy", "they're entitled", "of socialism", "they hate success", "they hate themselves", "they take too many vacations", "they don't take enough vacations", "I said so", "they were given participation trophies", "they're gay", "of smartphones", "of SJWs", "they live with their parents", "they leak government secrets", "they watch too much Netflix"];
   const things = ["entitlement", "communism", "kale", "excessive vacations", "social media", "selfies", "memes", "feelings", "participation trophies", "crowdfunding", "sexting", "the homosexual agenda", "the liberal agenda", "smartphones", "Pepe", "hashtags", "avocados", "emojis", "Netflix and chill"];
 
-  function getHeadline() {
-    let verb = () => verbs[Math.floor(Math.random()*verbs.length)];
-    let noun = () => nouns[Math.floor(Math.random()*nouns.length)];
-    let reason = () => reasons[Math.floor(Math.random()*reasons.length)];
-    let thing = () => things[Math.floor(Math.random()*things.length)];
+  function random(array) {
+    return array[Math.floor(Math.random()*array.length)]
+  }
 
-    const headlines = ["Millennials are " + verb() + " " + noun() + " because " + reason(), "Millennials are " + verb() + " " + noun() + " with " + thing(), "Millennials hate " + noun() + " because " + reason()];
+  function getHeadline() { // doesn't work well when headlines is global
+    const headlines = ["Millennials are " + random(verbs) + " " + random(nouns) + " because " + random(reasons), "Millennials are " + random(verbs) + " " + random(nouns) + " with " + random(things), "Millennials hate " + random(nouns) + " because " + random(reasons)];
 
-    let headline = () => headlines[Math.floor(Math.random()*headlines.length)];
-
-    return headline();
+    return random(headlines);
   }
 
   $("#headline").text(getHeadline());
